365b120bb074b6042739d529d120150ffd446301
Daniel Gracia <alonsod80@yahoo.com>
2023-02-01 22:46:00 -0500

    Basic NPC movement

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
 const canvas = document.getElementById("canvas1")
 const ctx = canvas.getContext("2d")
 
-const CANVAS_WIDTH = canvas.width = 600
-const CANVAS_HEIGHT = canvas.height = 600
+const CANVAS_WIDTH = canvas.width = 500
+const CANVAS_HEIGHT = canvas.height = 1000
 
-const playerImage = new Image()
-playerImage.src = "shadow_dog.png"
+const enemies = []
+const numberEnemies = 100
+
+let gameFrame = 0
+
+class Enemy{
+    constructor(imageSrc){
+        this.enemyImage = new Image()
+        this.enemyImage.src = imageSrc
+        this.x = Math.random() * CANVAS_WIDTH
+        this.y = Math.random() * CANVAS_HEIGHT
+        this.speed = Math.random()*4-2
+        this.spriteWidth = 293
+        this.spriteHeight = 155
+        this.width = this.spriteWidth*0.5
+        this.height = this.spriteHeight*0.5
+        this.frame = 0
+        this.flapSpeed = Math.floor(Math.random() * 4 + 2)
+    }
+    update(gameFrame){
+        this.x += this.speed
+        this.y += this.speed
+        if (gameFrame%this.flapSpeed===0){
+            this.frame > 4 ? this.frame = 0 : this.frame++
+        }
+            
+    }
+    draw(ctx){
+        ctx.drawImage(this.enemyImage,this.frame*this.spriteWidth,0,this.spriteWidth,this.spriteHeight,this.x,this.y,this.width,this.height)
+    }
+}
+
+for (let i = 0; i < numberEnemies; ++i) {
+    enemies.push(new Enemy("enemy1.png"))
+}
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
-    ctx.fillRect(50,50,100,100)
+    enemies.forEach(enemy => {
+        enemy.update(gameFrame)
+        enemy.draw(ctx)
+    })
+    gameFrame++
     requestAnimationFrame(animate)
 }
 animate()
