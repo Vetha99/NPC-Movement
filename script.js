b9337bac45c1f90a43904d72241ad4ec9e47aca7
Daniel Gracia <alonsod80@yahoo.com>
2023-02-03 13:29:31 -0500

    Random chaotic jumps NPC movement

--- script.js
+++ script.js
@@ -13,24 +13,19 @@ class Enemy{
     constructor(imageSrc){
         this.enemyImage = new Image()
         this.enemyImage.src = imageSrc
-        this.x = Math.random() * CANVAS_WIDTH
-        this.y = Math.random() * CANVAS_HEIGHT
-        this.speed = (Math.random()*4-2)*0.5
+        this.speed = (Math.random()*4+1)
         this.spriteWidth = 293
         this.spriteHeight = 155
         this.flapSpeed = Math.floor(Math.random() * 4 + 2)
         this.width = this.spriteWidth*this.flapSpeed*0.1
         this.height = this.spriteHeight*this.flapSpeed*0.1
+        this.x = Math.random() * (CANVAS_WIDTH - this.width)
+        this.y = Math.random() * (CANVAS_HEIGHT- this.height)
         this.frame = 0
-        this.angle = Math.atan2(this.x-500,this.y-500)
-        this.distance = Math.sqrt((this.x-500)**2+(this.y-500)**2)
-        const dir = Math.floor(Math.random()*2) > 0 ? 1 : -1
-        this.aumentAngle = (10/this.flapSpeed)/this.distance*dir
     }
     update(gameFrame){
-        this.angle+=this.aumentAngle
-        this.x = Math.cos(this.angle)*this.distance+500
-        this.y = Math.sin(this.angle)*this.distance+500
+        this.x += (Math.random()*1-0.5)*this.speed
+        this.y += (Math.random()*1-0.5)*this.speed
         if (gameFrame%this.flapSpeed===0){
             this.frame > 4 ? this.frame = 0 : this.frame++
         }
@@ -38,7 +33,6 @@ class Enemy{
     }
     draw(ctx){
         ctx.drawImage(this.enemyImage,this.frame*this.spriteWidth,0,this.spriteWidth,this.spriteHeight,this.x,this.y,this.width,this.height)
-        ctx.strokeRect(this.x,this.y,this.width,this.height)        
     }
 }
 
