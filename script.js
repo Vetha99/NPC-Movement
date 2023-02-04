7a9d2196f4334d11a2ccfca7785cc9ce6f6467a0
Daniel Gracia <alonsod80@yahoo.com>
2023-02-03 22:01:53 -0500

    Falling and rotating NPC movement

--- script.js
+++ script.js
@@ -13,35 +13,37 @@ class Enemy{
     constructor(imageSrc){
         this.enemyImage = new Image()
         this.enemyImage.src = imageSrc
-        this.speed = (Math.random()*4+1)
+        this.speed = (Math.random()*8+1)
         this.spriteWidth = 213
         this.spriteHeight = 213
         this.flapSpeed = Math.floor(Math.random() * 4 + 2)
         this.width = this.spriteWidth*this.flapSpeed*0.1
         this.height = this.spriteHeight*this.flapSpeed*0.1
-        this.x = Math.random() * (CANVAS_WIDTH - this.width)
+        this.x = Math.random() * (CANVAS_WIDTH - this.width)+this.width/2
         this.y = Math.random() * (CANVAS_HEIGHT- this.height)
-        this.newx = Math.random() * (CANVAS_WIDTH - this.width)
-        this.newy = Math.random() * (CANVAS_HEIGHT- this.height)
         this.frame = 0
         this.intervalMov = Math.floor(Math.random()*200)+50
+        this.angle = Math.random() *Math.PI
+        this.angledir = Math.random() > 0.5 ? 1 : -1
     }
     update(gameFrame){
-        if (gameFrame%this.intervalMov===0){
-            this.newx = Math.random() * (CANVAS_WIDTH - this.width)
-            this.newy = Math.random() * (CANVAS_HEIGHT- this.height)
+        this.angle+=Math.PI/100*this.angledir
+        this.y += this.speed
+        if (this.y-this.height/2>CANVAS_HEIGHT) {
+            this.y = -this.height
+            this.x = Math.random() * (CANVAS_WIDTH - this.width)+this.width/2
         }
-        const dx = this.x - this.newx
-        const dy = this.y - this.newy
-        this.x -= dx/100*this.speed
-        this.y -= dy/100*this.speed
         if (gameFrame%this.flapSpeed===0){
             this.frame > 7 ? this.frame = 0 : this.frame++
         }
             
     }
     draw(ctx){
-        ctx.drawImage(this.enemyImage,this.frame*this.spriteWidth,0,this.spriteWidth,this.spriteHeight,this.x,this.y,this.width,this.height)
+        ctx.save()
+        ctx.translate(this.x,this.y)
+        ctx.rotate(this.angle)
+        ctx.drawImage(this.enemyImage,this.frame*this.spriteWidth,0,this.spriteWidth,this.spriteHeight,0-this.width/2,0-this.height/2,this.width,this.height)
+        ctx.restore()
     }
 }
 
